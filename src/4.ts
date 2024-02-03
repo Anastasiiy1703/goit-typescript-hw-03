514a02827893c32fb634bcf45d75b7bbb9423022
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2024-02-03 17:52:12 +0200

    commit1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,51 @@
-const key = new Key();
+class Key {
+  
+}
 
+class MyHouse {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  openDoor(personKey: Key) {
+    if (this.key === personKey) {
+      console.log("Door opened");
+    } else {
+      console.log("Access denied");
+    }
+  }
+
+  comeIn(person: Person) {
+    console.log(`${person.getName()} came into the house`);
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+
+  getName() {
+    return "John"; 
+  }
+}
+
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
+
 
 
 export {};
