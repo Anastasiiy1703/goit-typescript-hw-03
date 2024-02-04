0505eb304042e3039c95335b3f9dedcf4e6a2dea
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2024-02-04 17:24:58 +0200

    commit3

--- src/4.ts
+++ src/4.ts
@@ -1,51 +1,51 @@
 class Key {
-  
-}
+  private signature: number = Math.random();
 
-class MyHouse {
-  private key: Key;
-
-  constructor(key: Key) {
-    this.key = key;
-  }
-
-  openDoor(personKey: Key) {
-    if (this.key === personKey) {
-      console.log("Door opened");
-    } else {
-      console.log("Access denied");
-    }
-  }
-
-  comeIn(person: Person) {
-    console.log(`${person.getName()} came into the house`);
+  getSignature(): number {
+    return this.signature;
   }
 }
 
 class Person {
-  private key: Key;
+  constructor(private key: Key) {}
 
-  constructor(key: Key) {
-    this.key = key;
-  }
-
-  getKey() {
+  public getKey(): Key {
     return this.key;
-  }
-
-  getName() {
-    return "John"; 
   }
 }
 
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open.");
+    } else {
+      console.log("Incorrect key, door is closed.");
+    }
+  }
+}
 
 const key = new Key();
+
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
+
 house.comeIn(person);
-
-
 
 export {};
